87ee8a6244de55e8cae3193544f79eeca56e5d57
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:31:03 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ 071-TesteResultConsole/071-TesteResultConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _071_TesteResultConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<String> task = new Task<String>
+            (
+                () =>
+                {
+                    return DateTime.Now.DayOfWeek.ToString();
+                }
+            );
+            
+            task.Start();
+
+            Console.WriteLine(task.Result);
+            Console.ReadKey();
+        }
+    }
+}

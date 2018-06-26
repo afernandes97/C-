31b899525fc37dc2bb42e22b9256adb2566e47fa
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:44:19 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //TRYCATCH
+            try
+            {
+                Console.Write("Digite um numero inteiro: ");
+                var x = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine(1000 / x);
+
+
+            }
+            catch ()
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //ex.GetType().FullName = traz o tipo do erro
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+
+                //throw;
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}

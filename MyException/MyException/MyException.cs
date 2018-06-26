e4817d1359259d1fae649644d70ec3bd7a0e385e
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:38:31 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ MyException/MyException/MyException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyException
+{
+    public class MyException : Exception
+    {
+        public MyException()//constructor
+        {
+            Logar();
+        }
+
+        private void Logar()
+        {
+            var arquivo = new StreamWriter("C:\\Users\\Admin\\Desktop\\teste\\log.txt", true, Encoding.Unicode);
+
+            arquivo.WriteLine("Probrema no programa");
+            arquivo.Close();
+            arquivo.Dispose();//liberar recursos sistema operacional
+        }
+    } 
+}

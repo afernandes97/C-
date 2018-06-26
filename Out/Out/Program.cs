9a726c74645917026fc541184aaec619259e27d8
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:41:09 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ Out/Out/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20, soma = 0, produto = 0;
+
+            CalcularSomaProduto(x, y, out soma, out produto);
+
+            Console.WriteLine(soma);
+            Console.WriteLine(produto);
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+        }
+
+        static void CalcularSomaProduto(int x, int y, out int soma, out int produto)
+        {
+            soma = x + y;
+            produto = x * y;
+        }
+    }
+}

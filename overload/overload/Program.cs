ef9cee178ba539277e28cc8dd33916eb3d44ef68
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:42:18 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ overload/overload/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // SOBRECARGA DE METODO
+            
+            var objetoSomador = new Somadora();
+            
+            //Console.WriteLine(objetoSomador.Somar(2,2));
+            //Console.WriteLine(objetoSomador.Somar(1,2,3));
+
+
+            //declarar array
+            int[] x;
+            //instanciar array
+            x = new int[3];
+
+            //atribuicao de valor
+            x[1] = 10;
+
+            Console.WriteLine(x[0]);// lendo array
+            Console.WriteLine(x[1]);
+            Console.WriteLine(x[2]);
+
+
+            //foreach (var item in x) //foreach é so para leitura (read-only)
+            //{
+            //    Console.WriteLine("usando for each: {0}", item);
+            //}
+
+            for (int i = 0; i < x.Length; i++)//percorrer (read-write) for para alteracao de valor
+            {   //length pega a quantidade de item do array
+                x[i] = (int)Math.Pow(2, x[i]);
+                Console.WriteLine("usando for: {0}", x[i]);
+            }
+
+            Console.WriteLine(objetoSomador.Somar(x));
+
+            var y = new int [] {1,2,3,4,5,6,7,8};
+
+            Console.WriteLine(objetoSomador.Somar(y));
+            Console.ReadKey();
+
+        }
+    }
+
+    // F11 - STEP IN
+    // F10 - STEP OVER
+
+    class Somadora
+    {
+        //internal int Somar(int x, int y)
+        //{
+        //    return x + y;
+        //}
+
+        internal double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        internal int Somar(params int[] x)
+        {
+            var retorno = 0;
+
+            foreach (var item in x)
+            {
+                retorno += item;
+            }
+            return retorno;
+        }
+    }
+
+}

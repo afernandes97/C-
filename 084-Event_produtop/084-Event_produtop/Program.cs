e3c882a9a2b1dc1a5552e728d6bf372932ac924e
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:47:37 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ 084-Event_produtop/084-Event_produtop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _084_Event_produtop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Produto
+            {
+                Codigo = 1,
+                Nome = "André",
+                EstoqueMinimo = 10,
+                Estoque = 20
+            };
+            //p.EventoEstoque += new DelegateEstoque(TratarEstoque);
+            p.EventoEstoque += TratarEstoque;
+
+            p.Estoque = 1;
+           
+            Console.ReadKey();
+        }
+
+        private static void TratarEstoque(Produto p)
+        {
+            Console.WriteLine("Estoque ({0}) do produto {1}, esta abaixo do minimo do recomendado({2})",p.Estoque, p.Nome ,p.EstoqueMinimo);
+        }
+
+        delegate void DelegateEstoque(Produto p);
+
+        class Produto
+        {
+            //event
+            //metodo associado - delegateestoque
+            public event DelegateEstoque EventoEstoque;
+
+            public int Codigo { get; set; }
+            public String Nome { get; set; }
+            public int EstoqueMinimo { get; set; }
+
+            private int estoque;
+
+
+            public int Estoque {
+                get { return estoque;  }
+                set { 
+                    estoque = value;
+                    if (EventoEstoque != null && estoque < EstoqueMinimo)
+                    {
+                        EventoEstoque(this);
+                    }
+                  }
+            }
+        }
+
+    }
+}

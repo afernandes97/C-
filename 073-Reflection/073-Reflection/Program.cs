18fbb1a5e4ff977606de4ab2b136f009be4dfa47
afernandes97 <30484386+afernandes97@users.noreply.github.com>
2018-06-26 15:32:49 -0300

    20483Programando C# Visual Studio Ka Solution

---
+++ 073-Reflection/073-Reflection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _073_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj;
+
+            Console.WriteLine("Que tipo de objeto voce quer criar? [S]String, [I]Int, [R]Random");
+            var qual = Console.ReadLine().ToUpper()[0];
+
+            switch (qual)
+            {
+                case 'S': obj = "Andre"; break;
+                case 'I': obj = 10 ; break;
+                case 'R': obj = new Random(); break;
+                default: return;                
+            }
+
+            var t = obj.GetType();
+
+            Console.WriteLine(t);
+            //type
+            Console.WriteLine(t.Namespace);
+            Console.WriteLine(t.Name);
+            Console.WriteLine(t.IsValueType);
+
+            Console.WriteLine("Field");
+
+            foreach (var item in t.GetFields())
+            {
+                Console.WriteLine("{0} => {1}", item, item.GetValue(obj));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 073-Reflection/073-Reflection/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("073-Reflection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("073-Reflection")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c4817ef-360d-4ea7-ae85-a24ad93caa77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
